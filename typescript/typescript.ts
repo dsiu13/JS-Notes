f489a22469d0af25f1b7cff235020c283ed412ef
IamZombiez <dereksiu216@gmail.com>
2018-09-25 19:02:00 -0700

    ts

--- typescript/typescript.ts
+++ typescript/typescript.ts
@@ -53,6 +53,58 @@ let sing = (): void => {
 // Never
 // Never tests two things: the 1st is that a function never returns anything.
 // The 2nd is that a var under a type guard is never true
+// You can never have a functional end point with never
 let error = (): never => {
   throw Error('Whoops');
 }
+
+// interface
+// ? - may or may not be part of the Object
+interface RobotArmy {
+    count: number,
+    type: string,
+    magic?: string
+}
+
+let fightRobotArmy = (robots: RobotsArmy)  => {
+  console.log("Fight")
+}
+
+// Type
+type RobotArmy {
+    count: number,
+    type: string,
+    magic: string
+}
+
+// Type Assertions
+// You can override a type in any way you want
+type CatArmy {
+    count: number,
+    type: string,
+    magic: string
+}
+
+let dog = {} as CatArmy
+dog.count
+
+// Function
+let fightRobotArmy = (robots: RobotsArmy): void  => {
+  console.log("Fight")
+}
+
+// Classes
+class Animal {
+  private sing: string = 'lalalalla'
+  constructor(sound:string){
+    this.sing = sound;
+  }
+
+  greet() {
+    return 'Hello ${this.sing}'
+  }
+
+}
+
+let lion = new Animal("Rawr")
+lion.greet()
